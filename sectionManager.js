1e20ea1d39074d21d60bb42f557d9e1be1449d45
Carlos Bajo <carlosabpreciado@gmail.com>
2018-08-22 11:42:40 -0600

    Scrolling top and down detected

--- sectionManager.js
+++ sectionManager.js
@@ -2,28 +2,42 @@
 var pages = document.getElementsByClassName('page');
 var parent = document.getElementById('main-container');
 var child = document.getElementById('sub-container');
-
-console.log(document);
-
-console.log(pages);
-
-window.onscroll = function (e) {
-    console.log('Me están haciendo scroll');
-}
-
-
-//Disable the scrollEvent...
-console.log(child);
-console.log(parent);
-
-
-console.log(child.offsetWidth);
-console.log(child.clientWidth);
-
-
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 var whiteSpace = 0;
-whiteSpace = child.offsetWidth - child.clientWidth;
-console.log(whiteSpace);
 
+//Disabling scroll
+whiteSpace = child.offsetWidth - child.clientWidth;
 child.style.paddingRight = whiteSpace + 'px';
 child.setAttribute("style", 'width:' + (child.clientWidth + whiteSpace) + 'px;');
+
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+
+    //Scrolling UP
+    if (e.deltaY < 0) {
+        //Code for scrolling UP...
+
+
+    } else {
+        //Code for scrolling DOWN...
+    }
+
+    e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+if (window.addEventListener) // older FF
+    window.addEventListener('DOMMouseScroll', preventDefault, false);
+window.onwheel = preventDefault; // modern standard
+window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+window.ontouchmove = preventDefault; // mobile
+document.onkeydown = preventDefaultForScrollKeys;
