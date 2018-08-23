5a6234867f4c1242226bd023a230a7c14b91ebce
Carlos Bajo <carlosabpreciado@gmail.com>
2018-08-22 22:35:26 -0600

    Up and down

--- sectionManager.js
+++ sectionManager.js
@@ -10,7 +10,7 @@ whiteSpace = child.offsetWidth - child.clientWidth;
 child.style.paddingRight = whiteSpace + 'px';
 child.setAttribute("style", 'width:' + (child.clientWidth + whiteSpace) + 'px;');
 
-
+var isScrolling = false;
 function preventDefault(e) {
     e = e || window.event;
     if (e.preventDefault)
@@ -18,15 +18,60 @@ function preventDefault(e) {
 
     //Scrolling UP
     if (e.deltaY < 0) {
-        //Code for scrolling UP...
-
-
+        movePage(true)
     } else {
-        //Code for scrolling DOWN...
+        movePage(false);
+    }
+
+    function movePage(orientation) {
+        if (!isScrolling) {
+            animatePages(pages, orientation, isScrolling);
+            isScrolling = true;
+            waitForScroll().then(function () {
+                isScrolling = false
+            });
+        }
+    }
+
+    function waitForScroll() {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve(true);
+            }, 1000);
+        });
     }
 
     e.returnValue = false;
 }
+
+function animatePages(pages, scrollingUp) {
+    var currentPage = pages.length - 1;
+    var pos = 0;
+    var pos2 = 350;
+    var id = setInterval(move, 0.001);
+    function move() {
+        if (scrollingUp) {
+            if (pos2 == 0) {
+                clearInterval(id);
+            } else {
+                pos2 -= 7;
+                pages[currentPage].style.top = pos2 + 'px';
+                //pages[currentPage].style.left = pos + 'px';
+            }
+        } else {
+            if (pos == 350) {
+                clearInterval(id);
+            } else {
+                pos += 7;
+                pages[currentPage].style.top = pos + 'px';
+                //pages[currentPage].style.left = pos + 'px';
+            }
+        }
+        
+    }
+
+}
+
 
 function preventDefaultForScrollKeys(e) {
     if (keys[e.keyCode]) {
