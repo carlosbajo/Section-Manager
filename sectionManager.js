9358bbaefbd0eabbba2f38e4319e356d2968406b
Carlos Bajo <carlosabpreciado@gmail.com>
2018-08-28 13:53:54 -0600

    Implementing the data

--- sectionManager.js
+++ sectionManager.js
@@ -23,26 +23,27 @@ function preventDefault(e) {
         movePage(false);
     }
 
-    function movePage(orientation) {
-        if (!isScrolling) {
-            animatePages(pages, orientation, isScrolling);
-            isScrolling = true;
-            waitForScroll().then(function () {
-                isScrolling = false
-            });
-        }
-    }
-
-    function waitForScroll() {
-        return new Promise(function (resolve, reject) {
-            setTimeout(function () {
-                resolve(true);
-            }, 1000);
-        });
-    }
-
     e.returnValue = false;
 }
+
+function movePage(orientation) {
+    if (!isScrolling) {
+        animatePages(pages, orientation, isScrolling);
+        isScrolling = true;
+        waitForScroll().then(function () {
+            isScrolling = false
+        });
+    }
+}
+
+function waitForScroll() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(true);
+        }, 1000);
+    });
+}
+
 
 var currentPage = pages.length - 1;
 var pageHeight = pages[0].offsetWidth;
@@ -74,7 +75,7 @@ function animatePages(pages, scrollingUp) {
                 currentPage += 1;
             } else {
                 pos2 = (pos2 - animationSmooth <= 0) ? 0 : pos2 -= animationSmooth;
-                pages[currentPage+1].style.right = pos2 + 'px';
+                pages[currentPage + 1].style.right = pos2 + 'px';
             }
         } else {
             if (pos >= pageHeight) {
@@ -91,22 +92,22 @@ function animatePages(pages, scrollingUp) {
 }
 
 // credit: http://www.javascriptkit.com/javatutors/touchevents2.shtml
-function swipedetect(el, callback){
-  
+function swipedetect(el, callback) {
+
     var touchsurface = el,
-    swipedir,
-    startX,
-    startY,
-    distX,
-    distY,
-    threshold = 150, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
-    elapsedTime,
-    startTime,
-    handleswipe = callback || function(swipedir){}
-  
-    touchsurface.addEventListener('touchstart', function(e){
+        swipedir,
+        startX,
+        startY,
+        distX,
+        distY,
+        threshold = 150, //required min distance traveled to be considered swipe
+        restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+        allowedTime = 300, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime,
+        handleswipe = callback || function (swipedir) { }
+
+    touchsurface.addEventListener('touchstart', function (e) {
         var touchobj = e.changedTouches[0];
         swipedir = 'none';
         startX = touchobj.pageX;
@@ -114,22 +115,22 @@ function swipedetect(el, callback){
         startTime = new Date().getTime() // record time when finger first makes contact with surface
         e.preventDefault()
     }, false)
-  
-    touchsurface.addEventListener('touchmove', function(e){
+
+    touchsurface.addEventListener('touchmove', function (e) {
         e.preventDefault() // prevent scrolling when inside DIV
     }, false)
-  
-    touchsurface.addEventListener('touchend', function(e){
+
+    touchsurface.addEventListener('touchend', function (e) {
         var touchobj = e.changedTouches[0]
         distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
         distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
         elapsedTime = new Date().getTime() - startTime // get time elapsed
-        if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+        if (elapsedTime <= allowedTime) { // first condition for awipe met
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+                swipedir = (distX < 0) ? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
             }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+                swipedir = (distY < 0) ? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
             }
         }
         handleswipe(swipedir)
@@ -154,8 +155,18 @@ function preventDefaultForScrollKeys(e) {
 }
 
 //USAGE
-swipedetect(parent, function(swipeOrientation) {
+swipedetect(parent, function (swipeOrientation) {
     console.log(swipeOrientation);
+    
+    switch (swipeOrientation) {
+        case 'right': case 'down':
+            movePage(false);
+            break;
+
+        case 'left': case 'up':
+            movePage(true);
+            break;
+    }
 });
 
 if (window.addEventListener) // older FF
